4cfb896a4344433c79015e195cbddc3c215aa7e0
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-14 13:17:41 -0700

    first commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿using BankEncapsulation;
+var bankaccount1 = new BankAccount();
+Console.WriteLine($"Hello, your current balance is {bankaccount1.GetBalance()}");
+Console.WriteLine($"Would you like to make a deposit?");
+Console.WriteLine("Please enter your deposit amount");
+double x = double.Parse(Console.ReadLine());
+bankaccount1.Deposit(x);
+Console.WriteLine($"Your new balance is {bankaccount1.GetBalance()}");
+
